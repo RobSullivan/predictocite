5440162cc061354642ef383ea7160c30aa50cb5b
Robert Sullivan <robertjsullivan.esq@gmail.com>
2015-07-28 22:46:57 +0100

    Added coverage tool and generated report in tmp.

--- manage.py
+++ manage.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 import os
+
+
 from app import create_app, db
 from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+
+
+COV = None
+
+if os.environ.get('FLASK_COVERAGE'):
+	import coverage
+	COV = coverage.coverage(branch=True, include='app/*')
+	COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -16,15 +26,30 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 @manager.command
-def test():
+def test(coverage=False):
 	"""Run the unit tests."""
 	#import unittest
 	#tests = unittest.TestLoader().discover('tests')
 	#unittest.TextTestRunner(verbosity=2).run(tests)
+	if coverage and not os.environ.get('FLASK_COVERAGE'):
+		import sys
+		os.environ['FLASK_COVERAGE'] = '1'
+		os.execvp(sys.executable, [sys.executable]+sys.argv)
+
 	import nose
 	tests = nose.loader.TestLoader().discover('tests')
 	nose.core.TextTestRunner(verbosity=1).run(tests)
 	#nose.core.run(suite=tests)
+	if COV:
+		COV.stop()
+		COV.save()
+		print('Coverage Summary:')
+		COV.report()
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		covdir = os.path.join(basedir, 'tmp/coverage')
+		COV.html_report(directory=covdir)
+		print('HTML version: file://%s/index.html' %covdir)
+		COV.erase()
 	
 
 
