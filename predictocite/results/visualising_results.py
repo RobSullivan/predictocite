d18e63b267152b93214c6c6b562bd8057226eba7
Robert Sullivan <robertjsullivan.esq@gmail.com>
2015-08-03 23:13:01 +0100

    Found and investigated SelectKBest to show top features form model.
    Not sure if this is what I need. Visualise these? Can set max features on vectorizer.

--- predictocite/results/visualising_results.py
+++ predictocite/results/visualising_results.py
@@ -18,6 +18,7 @@ from predictocite.datasets.citation_groups import fetch_citationgroups
 import numpy as np
 from sklearn import cross_validation, metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.naive_bayes import MultinomialNB
 
 
@@ -31,7 +32,7 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(
 
 #STEP 2: Extract features from text using TfidfVectorizer
 
-tfidf_vect = TfidfVectorizer(max_df=1, stop_words='english', ngram_range=(1, 2), encoding='utf-8')
+tfidf_vect = TfidfVectorizer(max_df=1, stop_words='english', ngram_range=(1, 2), encoding='utf-8', max_features=50000)
 
 """
 fit_transform learns the vocabulary dictionary
@@ -40,7 +41,7 @@ and return term-document matrix
 """
 
 X_train_tfidf = tfidf_vect.fit_transform(X_train)
-trained_features = tfidf_vect.get_feature_names() #a list
+feature_names = tfidf_vect.get_feature_names() #a list
 
 #STEP 3: train a classifer 
 
@@ -48,15 +49,35 @@ clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
 ###User submits data####
 
-user_doc = [X_test[0]]
+user_doc = [X_test[1]]
 
 #vectorized
 
 user_tfidf = tfidf_vect.transform(user_doc)
 
 #How do I get from user_tfidf matrix to look up features?
+# followed this tutorial http://scikit-learn.org/stable/auto_examples/text/document_classification_20newsgroups.html#example-text-document-classification-20newsgroups-py
+# uses SelectKBest univariate feature
 
+ch2 = SelectKBest(chi2, k=25)
 
+X_train_tfidf = ch2.fit_transform(X_train_tfidf, y_train)
 
+user_tfidf = ch2.transform(user_tfidf)
+
+#are these top features of user_tfidf or top feautres of X_train_tfidf?
+feature_names = [feature_names[i] for i in ch2.get_support(indices=True)]
+
+print(feature_names)
+
+"""
+X_test[0]
+['cdmrs', 'mapping copy', 'programs human', 'reveals changes', 'targeted bisulfi
+te', 'thousands genomes', 'title activates', 'title mapping', 'title seeker', 'v
+ariation population']
+
+"""
+
+np.asarray(feature_names)
 
 
